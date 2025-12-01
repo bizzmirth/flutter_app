7f91cd51d642d4aabbcad0a8826f77dae5f369eb
harshyadavDeveloper <harshyadav5939@gmail.com>
2025-12-01 11:55:43 +0530

    1. created folder structure of franchise

---
+++ lib/screens/dashboards/franchise/franchise.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FranchiseDashboardPage extends StatefulWidget {
+  const FranchiseDashboardPage({super.key});
+
+  @override
+  State<FranchiseDashboardPage> createState() => _FranchiseDashboardPageState();
+}
+
+class _FranchiseDashboardPageState extends State<FranchiseDashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
