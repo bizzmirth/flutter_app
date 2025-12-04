6e44d63c754c714d14d1a484623cae73bdbc2e49
harshyadavDeveloper <harshyadav5939@gmail.com>
2025-12-04 13:21:52 +0530

    1. added a data source for franchise customer 2. showed it in the ui

--- lib/screens/dashboards/franchise/customers/franchise_customer.dart
+++ lib/screens/dashboards/franchise/customers/franchise_customer.dart
@@ -1,15 +1,148 @@
+import 'package:bizzmirth_app/data_source/franchise_data_sources/fanchise_pending_customer_data_source.dart';
+import 'package:bizzmirth_app/main.dart';
+import 'package:bizzmirth_app/services/widgets_support.dart';
+import 'package:bizzmirth_app/widgets/filter_bar.dart';
 import 'package:flutter/material.dart';
 
 class FranchiseCustomer extends StatefulWidget {
   const FranchiseCustomer({super.key});
 
   @override
-  State<FranchiseCustomer> createState() => _FranchiseCustomerState();
+  State<FranchiseCustomer> createState() => _ViewCustomersPageState1();
 }
 
-class _FranchiseCustomerState extends State<FranchiseCustomer> {
+class _ViewCustomersPageState1 extends State<FranchiseCustomer> {
+  int _rowsPerPage = 5;
+  int _rowsPerPage1 = 5;
+  static const double dataRowHeight = 50.0;
+  static const double headerHeight = 56.0;
+  static const double paginationHeight = 60.0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'View Customers',
+          style: Appwidget.poppinsAppBarTitle(),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Divider(thickness: 1, color: Colors.black26),
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "All Pending Customer's List:",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const Divider(thickness: 1, color: Colors.black26),
+              const FilterBar(),
+
+              // Paginated Table for Pending List
+              Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: SizedBox(
+                  height: (_rowsPerPage * dataRowHeight) +
+                      headerHeight +
+                      paginationHeight,
+                  child: PaginatedDataTable(
+                    columnSpacing: 36,
+                    dataRowMinHeight: 40,
+                    columns: const [
+                      DataColumn(label: Text('Sr. No.')),
+                      DataColumn(label: Text('Name')),
+                      DataColumn(label: Text('Ref. ID')),
+                      DataColumn(label: Text('Ref. Name')),
+                      DataColumn(label: Text('Phone')),
+                      DataColumn(label: Text('Joining Date')),
+                      DataColumn(label: Text('Status')),
+                    ],
+                    source:
+                        FanchisePendingCustomerDataSource(orders, this.context),
+                    rowsPerPage: _rowsPerPage,
+                    availableRowsPerPage: const [5, 10, 15, 20, 25],
+                    onRowsPerPageChanged: (value) {
+                      if (value != null) {
+                        setState(() {
+                          _rowsPerPage = value;
+                        });
+                      }
+                    },
+                    arrowHeadColor: Colors.blue,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 35),
+              const Divider(thickness: 1, color: Colors.black26),
+              // Upcoming Events Section
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  "All Registered Customer's List:",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Divider(thickness: 1, color: Colors.black26),
+
+              // MyEmployeeRegDataSource
+
+              const FilterBar(),
+
+              // Paginated Table for Pending List
+              Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: SizedBox(
+                  height: (_rowsPerPage1 * dataRowHeight) +
+                      headerHeight +
+                      paginationHeight,
+                  child: PaginatedDataTable(
+                    columnSpacing: 36,
+                    dataRowMinHeight: 40,
+                    columns: const [
+                      DataColumn(label: Text('           ')),
+                      DataColumn(label: Text('ID')),
+                      DataColumn(label: Text('Full Name')),
+                      DataColumn(label: Text('Reg. ID')),
+                      DataColumn(label: Text('Reg. Name')),
+                      DataColumn(label: Text('Joining Date')),
+                      DataColumn(label: Text('Status')),
+                      DataColumn(label: Text('Action'))
+                    ],
+                    source: MyBMCustRegDataSource(orders1, this.context),
+                    rowsPerPage: _rowsPerPage1,
+                    availableRowsPerPage: const [5, 10, 15, 20, 25],
+                    onRowsPerPageChanged: (value) {
+                      if (value != null) {
+                        setState(() {
+                          _rowsPerPage1 = value;
+                        });
+                      }
+                    },
+                    arrowHeadColor: Colors.blue,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
