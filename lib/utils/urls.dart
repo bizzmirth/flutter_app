abe971696e1266390cbe3824231cbecaa3172635
harshyadavDeveloper <harshyadav5939@gmail.com>
2025-12-23 14:23:39 +0530

    1. added urls for franchise tc 2. added a franchise tc controller

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -225,4 +225,8 @@ class AppUrls {
   static String get getFranchiseeTopTravelConsultants => '$_franchisee/dashboard/top_travel_consultants_referral.php';
 
   static String get getFranchiseeCandidates => '$_franchisee/dashboard/candidate_count.php';
+
+  static String get getFranchiseePendingTc => '$_franchisee/travel_consultants/travel_consultants_pending.php';
+
+  static String get getFranchiseeRegisteredTc => '$_franchisee/travel_consultants/travel_consultants_registered.php';
 }
